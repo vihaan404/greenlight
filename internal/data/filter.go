371015c71d6cf4b98bab5e2799d7934112329443
Vihaan <vihaan2005yadav@gmail.com>
2024-10-21 12:41:04 +0530

    feature :sorting in getall method

--- internal/data/filter.go
+++ internal/data/filter.go
@@ -1,12 +1,32 @@
 package data
 
-import "github.com/vihaan404/greenlight/internal/validator"
+import (
+	"strings"
+
+	"github.com/vihaan404/greenlight/internal/validator"
+)
 
 type Filters struct {
 	Page         int
 	PageSize     int
 	Sort         string
 	SortSafelist []string
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	panic("unsafe sort paramenter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
 }
 
 func ValidateFilters(v *validator.Validator, f Filters) {
